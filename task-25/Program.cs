b75fd2a7c923fdc4aa79d28e1095b7827bd4a8f0
Sekai Alien <sekai.alien@mail.ru>
2022-11-10 17:54:43 +0500

    task 25 - degree of num  - cycle

---
+++ task-25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа:");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+
+int DegreeOfNum(int number)
+{
+    int result = 1;
+    int count = 0;
+    while (count < degree)
+    {
+        result *= number;
+        count++;
+    }
+    return result;
+}
+
+Console.WriteLine("Число " + num + " в степени " + degree + " равно " + DegreeOfNum(num));
+
